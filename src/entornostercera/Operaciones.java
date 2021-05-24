44d783b3158741f4e41c22267ec5d9f81fa67f58
d3st01 <d3st110@gmail.com>
2021-05-24 20:57:10 +0200

    Añadido el javadoc

--- src/entornostercera/Operaciones.java
+++ src/entornostercera/Operaciones.java
@@ -11,6 +11,22 @@ package entornostercera;
  */
 public class Operaciones {
 
+    /**
+     * recibe 2 parámetros de tipo int y retorna un array de tipo int. El
+     * resultado esperado tras la ejecución de dicho método es un array con
+     * todos los valores enteros comprendidos entre los dos parámetros (ambos
+     * incluidos) en orden ascendente (de menor a mayor). Si el primer parámetro
+     * es mayor que el segundo, debe generar igualmente el rango en orden
+     * ascendente.
+     *
+     * @author: Axel Ivan Valverde Pizarro
+     * @version: 1.1
+     * @since: 1.0
+     * @package: entornostercera
+     * @param a int
+     * @param b int
+     * @return nums int[]array
+     */
     public int[] rango(int a, int b) {
         int[] nums = new int[1];
         if (a == 0 && b == 0) {
@@ -31,6 +47,20 @@ public class Operaciones {
         return nums;
     }
 
+    /**
+     * recibe 2 parámetros de tipo int y retorna un entero. El resultado
+     * esperado como retorno es la suma de todos los valores comprendidos entre
+     * los dos parámetros (ambos incluidos). El método debe retornar el cálculo
+     * correcto con indiferencia del orden en el que reciba los dos argumentos.
+     *
+     * @author: Axel Ivan Valverde Pizarro
+     * @version: 1.1
+     * @since: 1.0
+     * @package: entornostercera
+     * @param: a int
+     * @param: b int
+     * @return sum int
+     */
     public int sumRango(int a, int b) {
         int sum = 0;
         if (a == b) {
@@ -49,6 +79,19 @@ public class Operaciones {
         return sum;
     }
 
+    /**
+     * recibe un parámetro de tipo int y devuelve un boolean. El resultado
+     * esperado tras la ejecución es true siempre que el valor del argumento sea
+     * par. Debe retornar false en caso contrario. Si el argumento es negativo o
+     * 0 se espera que el método propague la excepción IllegalArgumentException.
+     *
+     * @author: Axel Ivan Valverde Pizarro
+     * @version: 1.1
+     * @since: 1.0
+     * @package: entornostercera
+     * @param a int
+     * @return boolean
+     */
     public boolean isPar(int a) {
         if (a % 2 == 0) {
             if (a == 0) {
@@ -62,6 +105,20 @@ public class Operaciones {
         return false;
     }
 
+    /**
+     * recibe como parámetro un array de int y devuelve como resultado otro
+     * array de int. El resultado esperado es un nuevo array con los valores
+     * positivos del array recibido como parámetro. El nuevo array generado será
+     * el retorno del método. Si el array recibido es nulo, se espera que el
+     * método propague la excepción IllegalArgumentException. Si el array
+     * recibido está vacío, el array resultante también lo estará.
+     * @author: Axel Ivan Valverde Pizarro
+     * @version: 1.1
+     * @since: 1.0
+     * @package: entornostercera
+     * @param nums int[]array
+     * @return positivos 
+     */
     public int[] extraerPositivos(int[] nums) {
         if (nums == null) {
             throw new IllegalArgumentException();
